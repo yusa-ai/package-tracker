2efb327e1ab33a53fa7a3ff400444169a5675dbd
yusa-ai <ryan.malonzo@outlook.com>
2022-10-09 09:58:44 +0200

    Use Sheets API with gspread instead of Sheety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import gspread
 import requests
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -7,14 +8,28 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+
 TRACKING_URL = os.environ.get("TRACKING_URL")
+
+# Google Sheets
+GOOGLE_CREDENTIALS = {
+  "type": "service_account",
+  "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
+  "private_key_id": os.environ.get("GOOGLE_PRIVATE_KEY_ID"),
+  "private_key": os.environ.get("GOOGLE_PRIVATE_KEY"),
+  "client_email": os.environ.get("GOOGLE_CLIENT_EMAIL"),
+  "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": os.environ.get("GOOGLE_CLIENT_X509_CERT_URL")
+}
+GOOGLE_SPREADSHEET_NAME = "package-tracker"
 
 # Pushover
 APP_TOKEN = os.environ.get("APP_TOKEN")
 USER_KEY = os.environ.get("USER_KEY")
 
-# Sheety
-SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
 
 options = Options()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -49,22 +64,17 @@ timeline = [item for item in timeline if item.text != ""]
 last_update = timeline[0].text
 last_update = last_update.replace("\n", " ")
 
-# Read last status from Google Sheets
-response = requests.get(url=SHEETY_ENDPOINT)
-response.raise_for_status()
+# Initialize Google Spreadsheet
+gc = gspread.service_account_from_dict(GOOGLE_CREDENTIALS)
+sh = gc.open(GOOGLE_SPREADSHEET_NAME)
+worksheet = sh.get_worksheet(0)
 
-response = response.json()
-saved_status = response["packageTracker"][0]["response"]
-row_id = response["packageTracker"][0]["id"]
+# Read last status from Google Sheets
+saved_status = worksheet.acell("A1").value
 
 if last_update != saved_status:
     # Update Google Sheets
-    edit = requests.put(url=f"{SHEETY_ENDPOINT}/{row_id}", json={
-        "packageTracker": {
-            "response": last_update
-        }
-    })
-    edit.raise_for_status()
+    worksheet.update("A1", last_update)
 
     # Send push notification
     result = requests.post("https://api.pushover.net/1/messages.json", json={
